03fce1dcd8bf1f325e7b7738b71155381c770b93
Benedikt Beckermann <hello@benedikt.click>
2021-05-16 16:10:04 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-interface Color {
-    red: number
-    blue: Number
-    green: Number
-}
+export * from "./errors/bad-request-error"
+export * from "./errors/custom-error"
+export * from "./errors/database-connection-error"
+export * from "./errors/not-authorized-error"
+export * from "./errors/request-validation-error"
 
-const color: Color = {
-    red: 20,
-    blue: 10,
-    green: 10
-}
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-request'
 
-console.log(color)
-
-export default color
