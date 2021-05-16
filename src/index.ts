d91f0739b1dab2970ceddf4ecc91909bd5efb930
Benedikt Beckermann <hello@benedikt.click>
2021-05-16 16:01:50 +0200

    Updates

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number
+    blue: Number
+    green: Number
+}
+
+const color: Color = {
+    red: 20,
+    blue: 10,
+    green: 10
+}
+
+console.log(color)
+
+export default color
